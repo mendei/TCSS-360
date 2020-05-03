64a9fe6c08e5b041567b486faef34ea39bb3d9b5
Khaled <Khaled@10.0.0.5>
2020-05-03 11:27:42 -0700

    I have test nesw code

--- HelloWorld/src/Code/HelloTeam.java
+++ HelloWorld/src/Code/HelloTeam.java
@@ -9,3 +9,11 @@ public class HelloTeam {
 		System.out.println("Hello Anh!");
 	}
 }
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
