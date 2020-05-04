243be10a66c4036367f69d6694789131b3093d78
Tran Anh Tu <trananhtu171093@gmail.com>
2020-05-03 20:50:18 -0700

    Add classes and methods

--- AboutScreen/src/UserInterface.java
+++ AboutScreen/src/UserInterface.java
@@ -89,18 +89,60 @@ public class UserInterface extends JFrame {
 
         JPanel classAndMethodTitlePnl = new JPanel();
         classAndMethodTitlePnl.setBorder(BorderFactory.createLineBorder(Color.black));
-        classAndMethodTitlePnl.setBorder(BorderFactory.createLineBorder(Color.black));
         classAndMethodTitlePnl.add(new JLabel("Classes And Methods", JLabel.CENTER));
 
         JPanel classAndMethodContentPnl = new JPanel();
         classAndMethodContentPnl.setBorder(BorderFactory.createLineBorder(Color.black));
-        classAndMethodContentPnl.setLayout(new GridLayout(7, 1));
+        classAndMethodContentPnl.setLayout(new GridLayout(1, 2));
+        
+        JPanel adminClassPnl = new JPanel();
+        adminClassPnl.setLayout(new BorderLayout());
+        
+        JPanel adminNamePnl = new JPanel();
+        adminNamePnl.setBorder(BorderFactory.createLineBorder(Color.black));
+        adminNamePnl.add(new JLabel("Administrator"), JLabel.CENTER);
+        
+        JPanel adminMethodPnl = new JPanel();
+        adminMethodPnl.setBorder(BorderFactory.createLineBorder(Color.black));
+        adminMethodPnl.setLayout(new GridLayout(5,1));
+        adminMethodPnl.add(new JLabel(String.format("%10s+ addFile()", ""), JLabel.LEFT));
+        adminMethodPnl.add(new JLabel(String.format("%10s+ removeFile()", ""), JLabel.LEFT));
+        adminMethodPnl.add(new JLabel(String.format("%10s+ searchDocuments()", ""), JLabel.LEFT));
+        adminMethodPnl.add(new JLabel(String.format("%10s+ downloadDocument()", ""), JLabel.LEFT));
+        adminMethodPnl.add(new JLabel(String.format("%10s+ acceptVisitor()", ""), JLabel.LEFT));
+        
+        adminClassPnl.add(adminNamePnl, BorderLayout.NORTH);
+        adminClassPnl.add(adminMethodPnl, BorderLayout.CENTER);
+        
+        JPanel visitorClassPnl = new JPanel();
+        visitorClassPnl.setLayout(new BorderLayout());
+        
+        JPanel visitorNamePnl = new JPanel();
+        visitorNamePnl.setBorder(BorderFactory.createLineBorder(Color.black));
+        visitorNamePnl.add(new JLabel("Visitor"), JLabel.CENTER);
+        
+        JPanel visitorMethodPnl = new JPanel();
+        visitorMethodPnl.setBorder(BorderFactory.createLineBorder(Color.black));
+        visitorMethodPnl.setLayout(new GridLayout(5,1));
+        
+        visitorMethodPnl.add(new JLabel(String.format("%10s+ login()", ""), JLabel.LEFT));
+        visitorMethodPnl.add(new JLabel(String.format("%10s+ searchDocuments()", ""), JLabel.LEFT));
+        visitorMethodPnl.add(new JLabel(String.format("%10s+ downloadDocuments()", ""), JLabel.LEFT));
+        
+        visitorClassPnl.add(visitorNamePnl, BorderLayout.NORTH);
+        visitorClassPnl.add(visitorMethodPnl, BorderLayout.CENTER);
+        
+        classAndMethodContentPnl.add(adminClassPnl);
+        classAndMethodContentPnl.add(visitorClassPnl);
+        
+        /*
         classAndMethodContentPnl.add(new JLabel(""));
         classAndMethodContentPnl.add(new JLabel("Method class 1", JLabel.CENTER));
         classAndMethodContentPnl.add(new JLabel("Method class 2", JLabel.CENTER));
         classAndMethodContentPnl.add(new JLabel("Method class 3", JLabel.CENTER));
         classAndMethodContentPnl.add(new JLabel("Method class 4", JLabel.CENTER));
         classAndMethodContentPnl.add(new JLabel("..............", JLabel.CENTER));
+        */
 
         classAndMethodPnl.add(classAndMethodTitlePnl, BorderLayout.NORTH);
         classAndMethodPnl.add(classAndMethodContentPnl, BorderLayout.CENTER);
