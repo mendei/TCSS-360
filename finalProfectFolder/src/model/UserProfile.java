62a44c0b39ff2cfc33a362ebb777ab85fa331c59
tatu1710 <tatu1710@10.0.0.239>
2020-05-23 14:52:12 -0700

    Add userInfo text file and updated UserProfile

--- finalProfectFolder/src/model/UserProfile.java
+++ finalProfectFolder/src/model/UserProfile.java
@@ -1,48 +1,31 @@
 package model;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
 
+public class UserProfile extends JFrame {
 
-public class UserProfile extends JFrame{
-	
 	private String name1;
-	
+
 	private String email;
-	
+
 	public UserProfile() {
 
-		
 	}
-	
 
-	
-
-	
-
-	
 	public void setName(String theName) {
 		name1 = theName;
-		
 	}
-	
+
 	public void setEmail(String theEmail) {
 		email = theEmail;
 	}
-	
+
 	public String getName() {
 		return name1;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
 
-	
-	
 }
