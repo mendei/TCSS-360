43d20d9ebeaa692e66eaded3289f63a36e522e05
Ivan <Ivan@LAPTOP-9HPDFLH7>
2020-06-03 13:47:37 -0700

    home screen

---
+++ finalProjectFolder2/src/gui/HomeScreen.java
@@ -0,0 +1,74 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class HomeScreen {
+	
+public  void createWindow() {
+		
+		JFrame frame = new JFrame("Panel Example");
+		JPanel containPanel = new JPanel();
+		containPanel.setLayout(new BorderLayout());
+		
+		containPanel.add(taskbar(), BorderLayout.NORTH);
+		containPanel.add(fileDirectory(), BorderLayout.WEST);
+		containPanel.add(documentViewer(), BorderLayout.EAST);
+		
+		containPanel.setSize(750,600);
+		frame.add(containPanel);
+		frame.setSize(800,600);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+
+	public JPanel taskbar() {
+		
+		JPanel taskbarPanel = new JPanel();
+		JLabel label = new JLabel("Taskbar");
+		JButton searchButton = new JButton("Search");
+		JButton addButton = new JButton("Add");
+		
+		taskbarPanel.add(searchButton);
+		taskbarPanel.add(addButton);
+		taskbarPanel.add(label);
+		
+		return taskbarPanel;
+	}
+	
+	public JPanel fileDirectory() {
+		
+		JPanel fileDirectoryPanel= new JPanel();
+		fileDirectoryPanel.setBounds(40,80,600,400);
+		fileDirectoryPanel.setBackground(Color.gray);
+		JLabel label = new JLabel("File Directory");
+		fileDirectoryPanel.add(label);
+		
+		return fileDirectoryPanel;
+		
+	}
+	
+	public JPanel documentViewer() {
+		JPanel docViewerPanel = new JPanel();
+		JLabel label = new JLabel("Document Viewer");
+		
+		docViewerPanel.add(label);
+		return docViewerPanel;
+		
+	}
+	
+	public static void main(String args[]) {
+		
+		HomeScreen homeScreen = new HomeScreen();
+		homeScreen.createWindow();
+		
+	}
+
+	
+}
